e21d02232ad8095390b34505ad52f65e54a23d99
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-03-08 00:15:28 +0000

    Switch back to index.html.

--- source/javascripts/controllers/application_controller.js
+++ source/javascripts/controllers/application_controller.js
@@ -1,1 +1,10 @@
-App.ApplicationController = Ember.Controller.extend();
+App.ApplicationController = Ember.Controller.extend({
+  firstName: "Fayimora",
+  lastName: "Femi-Balogun",
+  nickname: "DaMainBoss",
+  buttonText: "My large button",
+
+  showMessage: function () {
+    alert("I am " + this.nickname);
+  }
+});

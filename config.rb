cbcf72bda23273c9ad14a84e81b33b495d65d894
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-03-09 02:03:12 +0000

    Add option to minify assets on build.

--- config.rb
+++ config.rb
@@ -65,10 +65,10 @@ set :css_assets_paths, ["#{root}/vendor/stylesheets/"]
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable asset hash
   # activate :asset_hash
@@ -77,7 +77,7 @@ configure :build do
   # activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
